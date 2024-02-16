d8d1b8ba32c1b3791a7d541c1705aab6d4d1d82e
Dede <deirfed@gmail.com>
2024-02-16 11:47:05 +0700

    feat: initiate navbar

--- resources/views/layout/navbar.blade.php
+++ resources/views/layout/navbar.blade.php
@@ -24,3 +24,63 @@
         </li>
     </ul>
 </li>
+
+<li class="nav-item dropdown">
+    <a class="nav-link dropdown-toggle" href="#" id="appsDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        <i class="icon-settings nav-icon"></i>
+        Pengaturan
+    </a>
+    <ul class="dropdown-menu" aria-labelledby="dashboardsDropdown">
+        <li>
+            <a class="dropdown-item" href="">Data Essentials</a>
+        </li>
+        <li>
+            <a class="dropdown-item" href="">Data Assets</a>
+        </li>
+    </ul>
+</li>
+
+<li class="nav-item dropdown">
+    <a class="nav-link dropdown-toggle" href="#" id="appsDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        <i class="icon-database nav-icon"></i>
+        Kelola Absensi
+    </a>
+    <ul class="dropdown-menu" aria-labelledby="dashboardsDropdown">
+        <li>
+            <a class="dropdown-item" href="">Data Essentials</a>
+        </li>
+        <li>
+            <a class="dropdown-item" href="">Data Assets</a>
+        </li>
+    </ul>
+</li>
+
+<li class="nav-item dropdown">
+    <a class="nav-link dropdown-toggle" href="#" id="appsDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        <i class="icon-info nav-icon"></i>
+        Informasi
+    </a>
+    <ul class="dropdown-menu" aria-labelledby="dashboardsDropdown">
+        <li>
+            <a class="dropdown-item" href="">Data Essentials</a>
+        </li>
+        <li>
+            <a class="dropdown-item" href="">Data Assets</a>
+        </li>
+    </ul>
+</li>
+
+<li class="nav-item dropdown">
+    <a class="nav-link dropdown-toggle" href="#" id="appsDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        <i class="icon-eye nav-icon"></i>
+        Informasi Gaji
+    </a>
+    <ul class="dropdown-menu" aria-labelledby="dashboardsDropdown">
+        <li>
+            <a class="dropdown-item" href="">Data Essentials</a>
+        </li>
+        <li>
+            <a class="dropdown-item" href="">Data Assets</a>
+        </li>
+    </ul>
+</li>
