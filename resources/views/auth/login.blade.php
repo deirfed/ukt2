2442e0ab125a4601ca782241879243c847a15373
Dede <deirfed@gmail.com>
2024-04-04 22:23:43 +0700

    feat: update login, request eko

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,22 +1,25 @@
 <!DOCTYPE html>
 <html lang="en">
 
-    <head>
-        <meta charset="UTF-8">
-        <title>Login | UKT2.ORG Kep. Seribu</title>
-        <link rel='stylesheet' href='https://use.fontawesome.com/releases/v5.2.0/css/all.css'>
-        <link rel='stylesheet' href='https://use.fontawesome.com/releases/v5.2.0/css/fontawesome.css'>
-        <link rel="stylesheet" href="{{ asset('assets/css/csslogin/style.css') }}">
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-            integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-        <link rel="shortcut icon" href="{{ asset('assets/img/ukt2logo.png') }}">
+<head>
+    <meta charset="UTF-8">
+    <title>Login | UKT2.ORG Kep. Seribu</title>
+    <link rel='stylesheet' href='https://use.fontawesome.com/releases/v5.2.0/css/all.css'>
+    <link rel='stylesheet' href='https://use.fontawesome.com/releases/v5.2.0/css/fontawesome.css'>
+    <link rel="stylesheet" href="{{ asset('assets/css/csslogin/style.css') }}">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="shortcut icon" href="{{ asset('assets/img/ukt2logo.png') }}">
 
-    </head>
+</head>
 
-    <body>
-        <div class="container">
-            <div class="screen">
-
+<body>
+    <div class="container">
+        <div class="screen">
+            <div class="img-back">
+                <div class="text-head">
+                    <h1 class="text-center">SIMOJA</h1>
+                </div>
                 <div class="screen__content">
                     <div class="title">
                         <img src="{{ asset('assets/img/ukt2logo.png') }}" alt="" class="login-logo">
@@ -26,8 +29,8 @@
                         @method('post')
                         <div class="login__field">
                             <i class="login__icon fas fa-user"></i>
-                            <input type="text" name="email" class="login__input" placeholder="input email" required
-                                autofocus>
+                            <input type="text" name="email" class="login__input" placeholder="Masukkan Email"
+                                required autofocus>
                         </div>
                         @error('email')
                             <span class="badge bg-danger p-2 mb-3" role="alert">
@@ -36,7 +39,7 @@
                         @enderror
                         <div class="login__field">
                             <i class="login__icon fas fa-lock"></i>
-                            <input type="password" name="password" class="login__input" placeholder="input password"
+                            <input type="password" name="password" class="login__input" placeholder="Masukkan Password"
                                 required>
                         </div>
                         @error('password')
@@ -50,15 +53,18 @@
                         </button>
                     </form>
                 </div>
-                <div class="screen__background">
-                    <span class="screen__background__shape screen__background__shape4"></span>
-                    <span class="screen__background__shape screen__background__shape3"></span>
-                    <span class="screen__background__shape screen__background__shape2"></span>
-                    <span class="screen__background__shape screen__background__shape1"></span>
-                </div>
             </div>
         </div>
+    </div>
+    <footer id="footer">
+        <p>&copy; <span id="currentYear"></span> SIMOJA. All Rights Reserved.</p>
+    </footer>
+</body>
 
-    </body>
+<script>
+    var currentYear = new Date().getFullYear();
+
+    document.getElementById("currentYear").textContent = currentYear;
+</script>
 
 </html>
