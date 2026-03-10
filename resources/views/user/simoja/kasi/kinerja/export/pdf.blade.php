b186805e29506ad57bfc24d63170b4da5276d245
Tio Muhamad Nur <outsider.kuningan52@gmail.com>
2026-03-10 21:59:59 +0700

    update: change pdf generator engine

--- resources/views/user/simoja/kasi/kinerja/export/pdf.blade.php
+++ resources/views/user/simoja/kasi/kinerja/export/pdf.blade.php
@@ -228,7 +228,7 @@
                 <tr>
                     <td>Periode</td>
                     <td>:</td>
-                    <td class="text-nowrap">{{ $start_date }} s/d {{ $end_date }}</td>
+                    <td style="white-space: nowrap;">{{ $start_date }} s/d {{ $end_date }}</td>
                 </tr>
             </table>
         </div>
